7ad091060fc3f200967c6ff5eb86d15bcf34dd49
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2017-09-15 19:25:25 +0300

    Panaikinta Form1 Papildyta Kavine.cs

--- CafeApp/CafeApp/Kavine.cs
+++ CafeApp/CafeApp/Kavine.cs
@@ -3,8 +3,9 @@ using System.Drawing.Image;
 
 public class Kavine
 {
+    private string _searchCode { get; } //read-only outside the class
+
     protected string _name;
-    protected string _searchCode;
     protected string _address;
     protected string _description;
     protected string _phoneNumber;
@@ -16,11 +17,38 @@ public class Kavine
 
     public Kavine(string name, string address, int tableCount)
     {
+        //TODO add checking if string != "", tableCount < 0
         _name = name;
         _address = address;
         _tableCount = tableCount;
         //_mainImage = some stock image
+        GenerateAndSet_searchCode();
     }
 
+    private void GenerateAndSet_searchCode()
+    {
+        string namePart;
+        if (_name.Length > 2)                   //jei vardas bent 3 raides
+        {                                       //paima 3 raides i _searchCode
+            namePart = _name.Substring(0, 3);
+        }
+        else                                    //kitu atveju sugeneruoja random 3 raides
+        {
+            namePart = RandomString(3);
+        }
 
+        string numberPart = RandomString(3);
+
+        _searchCode = namePart + "-" + numberPart;
+    }
+
+    //TODO: split to char string and numeric string
+    static string RandomString(int length)
+    {
+        Random random = new Random();
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        return new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
 }
