586c806c27a2efc5273de45a0a215d1eb75d142b
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2017-09-15 18:05:45 +0300

    Basic kavines klase

---
+++ CafeApp/CafeApp/Kavine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing.Image;
+
+public class Kavine
+{
+    protected string _name;
+    protected string _searchCode;
+    protected string _address;
+    protected string _description;
+    protected string _phoneNumber;
+    protected Image _mainImage;
+    //protected var locationVar; //placeholder if we decide to do anything with GoogleMaps
+    protected int _tableCount;
+
+    protected int availableTables;
+
+    public Kavine(string name, string address, int tableCount)
+    {
+        _name = name;
+        _address = address;
+        _tableCount = tableCount;
+        //_mainImage = some stock image
+    }
+
+
+}
