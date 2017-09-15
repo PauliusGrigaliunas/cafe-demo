09e31ce1943da0b42f15afea63f00b83b993cbad
KaSaras <skbzuvis@gmail.com>
2017-09-15 18:56:12 +0300

    Loaction system
    Able to find latitude and longtitude. Determining address from location still not implemented

--- CafeApp/CafeApp/Form1.cs
+++ CafeApp/CafeApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Device.Location;
 
 namespace CafeApp
 {
@@ -15,6 +16,46 @@ namespace CafeApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        GeoCoordinateWatcher watcher = null;
+        GeoCoordinate coordinate = null;
+        CivicAddressResolver resolver = null;
+        CivicAddress address = null;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            watcher = new GeoCoordinateWatcher();
+            coordinate = new GeoCoordinate();
+            resolver = new CivicAddressResolver();
+            address = new CivicAddress();
+            watcher.StatusChanged += watcher_StatusChanged;
+            watcher.Start();
+        }
+
+        private void watcher_StatusChanged(Object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            if (e.Status == GeoPositionStatus.Ready)
+            {
+                if (watcher.Position.Location.IsUnknown)
+                {
+                    textBox1.Text = "Location is unknown";
+                }
+                else
+                {
+                    coordinate = watcher.Position.Location;
+                    address = resolver.ResolveAddress(coordinate);
+
+                    
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            textBox1.Text =String.Format("Latitude:{0} Longtitude{1}",address.CountryRegion, address.City);
         }
     }
 }
