3f4d08c05fd8703ef7978aedf4c3e4b4af379c6a
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-09-17 19:20:46 +0300

    Revert "aa"
    This reverts commit c1867077b854baf775914240be326914a1d4b2c1.

--- CafeApp/CafeApp/Program.cs
+++ CafeApp/CafeApp/Program.cs
@@ -16,7 +16,7 @@ namespace CafeApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RegisterWindow());
+            Application.Run(new MainForm());
         }
     }
 }
