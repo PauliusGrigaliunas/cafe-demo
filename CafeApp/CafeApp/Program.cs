c1867077b854baf775914240be326914a1d4b2c1
Albert Jurkoit <31667609+albertJkt@users.noreply.github.com>
2017-09-17 18:37:16 +0300

    aa

--- CafeApp/CafeApp/Program.cs
+++ CafeApp/CafeApp/Program.cs
@@ -16,7 +16,7 @@ namespace CafeApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new RegisterWindow());
         }
     }
 }
