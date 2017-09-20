728cdc3ce081fe210d11f198406b8ae46e119080
KaSaras <sarunas.kazimieras.buteikis@gmail.com>
2017-09-20 23:11:51 +0300

    Add location to the project
    Moved the methods to find user location from the prototype to our main project. in my branch Will change /where/when the location should be gotten in the future.

---
+++ CafeApp/CafeApp/LoginForm.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Device.Location;
+using GoogleDirections;
+
+namespace CafeApp
+{
+    public partial class LoginForm : Form
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\KaSaras\Documents\GitHub\cafe-demo\CafeApp\CafeApp\Database1.mdf;Integrated Security=True");
+        public LoginForm()
+        {
+            InitializeComponent();
+            textBox2.PasswordChar = '*';
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connect.Open();                                                //prisijungiam prie db
+                SqlCommand cmd = connect.CreateCommand();                      // sukuriam komanda
+                cmd.CommandType = CommandType.Text;                             //nustatom komanda teksto tipo
+                cmd.CommandText = "SELECT * FROM Users WHERE Email='"+Email.Text+"'AND Password='"+textBox2.Text+"'";                        // parenkam visa lentele 
+                cmd.ExecuteNonQuery();                                           //vykdom komandine cmd eilute
+                DataTable dt = new DataTable();                                //sukuriam saugojimui duomenu lentele
+                SqlDataAdapter da = new SqlDataAdapter(cmd);                   //sukuriam sql duomenu adapteri, kurie jau yra isspausdinti cmd                           
+                da.Fill(dt);                                                //uzpildom duomenu lentele dt
+                int i = dt.Rows.Count;                                          //surandame ar yra nors viena eilute su tokiais duomenim
+                if ( i == 1 )                                             //jei yra 1, tada cool, prisijungta sekminga
+                {
+                    MainMenu main = new MainMenu(Email.Text);
+                    main.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    Email.Text = null;
+                    textBox2.Text = null;
+                    MessageBox.Show("Atsiprašome, bet el. pašto adresas arba slaptažodis neteisingi!");
+                }
+                connect.Close();                  //atsijungiam nuo db
+            }
+            catch ( Exception ex )
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            RegisterWindow reg = new RegisterWindow();
+            reg.ShowDialog();
+
+        }
+        /// <summary>
+        /// This part is subject to change
+        /// </summary>
+        Geocoder geocoder = new Geocoder("AIzaSyBCSrZJLMW4FiC85dC7TW6XSxBOFn7LGwU");
+        GeoCoordinateWatcher watcher = null; 
+        Dictionary<string, string> userLocation = null;
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            watcher = new GeoCoordinateWatcher();
+            watcher.StatusChanged += watcher_StatusChanged;
+            watcher.Start();
+        }
+
+        private void watcher_StatusChanged(Object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            if (e.Status == GeoPositionStatus.Ready)
+            {
+                if (watcher.Position.Location.IsUnknown)
+                {
+                   //Location is unknown
+                }
+                else
+                {
+                    /*Dictionary[key] = value
+                     ////////////////////
+                     [street_number] = street number
+                     [route] = street
+                     [locality] = city
+                     [administritive_are_level_2] = savivaldybe
+                     [administritive_are_level_1] = apskritis
+                     [country] = country
+                     [postal_code] = postal code
+                     [establishment] = establishment
+                     [bus_station] = bus station
+                     [neighbourhood] = neighbbourhood
+                     [administritive_are_level_3] = seniunija
+                     ///////////////////////////
+                     */
+                    userLocation = geocoder.ReverseGeocodeComponents(new LatLng(watcher.Position.Location.Latitude, watcher.Position.Location.Longitude));
+                }
+            }
+        }
+        /// <summary>
+        ///this part is subject to change
+        /// </summary>
+        
+
+    }
+}
---
+++ CafeApp/CafeApp/RegisterWindow.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace CafeApp
+{
+    public partial class RegisterWindow : Form
+    {
+
+        public string name;
+        public string surname;
+        public string e_mail;  // NOTE: Email = username
+        public string password;
+        public bool inserted=false;
+
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\KaSaras\Documents\GitHub\cafe-demo\CafeApp\CafeApp\Database1.mdf;Integrated Security=True");
+
+
+
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name = textBox1.Text;
+            surname = textBox2.Text;
+            e_mail = textBox3.Text;
+            password = textBox4.Text;
+            /// TODO: Validation system (check for valid email, password. 
+            /// After validating data INSTERT TO DB
+            /// Note: Don't forget about try/catch 
+            
+            if (IsValidEmail(e_mail)&& IsValidPassword(password))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT * FROM Users";
+                    cmd.ExecuteNonQuery();
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter sa = new SqlDataAdapter(cmd);
+                    sa.Fill(dt);
+                    if (!CheckIfMailExists(dt, textBox3.Text) && !inserted)
+                    {
+                        cmd.CommandText = "INSERT INTO Users (Email,Password,Name,Surname) VALUES('"+textBox3.Text+ "','" + textBox4.Text + "','" + textBox1.Text + "','" + textBox2.Text + "')";
+                        cmd.ExecuteNonQuery();
+                        inserted = true;
+                        MessageBox.Show("User successfully registered!");
+                        connection.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("User already exists, please choose different email or login into our system.");
+                        connection.Close();
+                    }
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Your email or password are incorrect, please, check them and try again.");
+            }
+            this.Close();
+        }
+
+        private bool IsValidEmail (string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private bool IsValidPassword (string password)
+        {
+            try
+            {
+                int length = password.Length;
+                return length >= 6;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private bool CheckIfMailExists(DataTable dt, string data)
+        {
+
+            bool exists = false;
+            for (int i=0; i < dt.Rows.Count; i++)
+            {
+                if (data == dt.Rows[i][0].ToString())
+                {
+                    exists = true;
+                }
+            }
+            return exists;
+        }
+    }
+}
