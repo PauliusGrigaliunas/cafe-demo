e8ad8d0bd3eb3d05f4398a068fca83de162132ba
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2017-09-20 23:21:31 +0300

    ++

--- CafeApp/CafeApp/ListForm.Designer.cs
+++ CafeApp/CafeApp/ListForm.Designer.cs
@@ -28,11 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listViewCafe = new System.Windows.Forms.ListView();
+            this.columnID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listViewCafe
+            // 
+            this.listViewCafe.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnID,
+            this.columnName});
+            this.listViewCafe.Location = new System.Drawing.Point(12, 12);
+            this.listViewCafe.Name = "listViewCafe";
+            this.listViewCafe.Size = new System.Drawing.Size(1192, 453);
+            this.listViewCafe.TabIndex = 0;
+            this.listViewCafe.UseCompatibleStateImageBehavior = false;
+            this.listViewCafe.View = System.Windows.Forms.View.Details;
+            // 
+            // columnID
+            // 
+            this.columnID.Text = "Id";
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(946, 499);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 578);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listViewCafe);
+            this.Name = "ListForm";
             this.Text = "ListForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewCafe;
+        private System.Windows.Forms.ColumnHeader columnID;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.Button button1;
     }
 }
