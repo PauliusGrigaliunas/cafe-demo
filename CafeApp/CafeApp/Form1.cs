a434a22b3c62b72f412e29666d8d98839ba793f7
KaSaras <sarunas.kazimieras.buteikis@gmail.com>
2017-09-27 18:23:47 +0300

    Revert "Complete reverse geocoding prototype"
    This reverts commit 0e4eaf74291c416131fb56b69ec001966cc43bf0.

--- CafeApp/CafeApp/Form1.cs
+++ CafeApp/CafeApp/Form1.cs
@@ -48,11 +48,10 @@ namespace CafeApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //returns a dictionary of full address
-            var fullAddress = geocoder.ReverseGeocodeComponents(new LatLng(coordinate.Latitude, coordinate.Longitude));
-
-            //prints the city and the country the user is currently in
-            textBox1.Text = String.Format("City: {0} Country {1}", fullAddress["locality"], fullAddress["country"]);
+            double Latitude = coordinate.Latitude;
+            double Longtitude = coordinate.Longitude;
+            String address = geocoder.ReverseGeocode(new LatLng(Latitude,Longtitude));
+            textBox1.Text =String.Format("Location: "+address);
         }
     }
 }
