4580318f637f38176e2ea79465018d4394bb26d8
KaSaras <sarunas.kazimieras.buteikis@gmail.com>
2017-09-27 22:02:36 +0300

    Add search by name
    Able to search a specific cafe by it's name. Putting in nothing will refresh the cafe list

--- CafeApp/CafeApp/ListForm.Designer.cs
+++ CafeApp/CafeApp/ListForm.Designer.cs
@@ -40,6 +40,8 @@
             this.columnSunday = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listViewCafe
@@ -58,9 +60,9 @@
             this.listViewCafe.Dock = System.Windows.Forms.DockStyle.Top;
             this.listViewCafe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
             this.listViewCafe.Location = new System.Drawing.Point(0, 0);
-            this.listViewCafe.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listViewCafe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listViewCafe.Name = "listViewCafe";
-            this.listViewCafe.Size = new System.Drawing.Size(1261, 465);
+            this.listViewCafe.Size = new System.Drawing.Size(946, 379);
             this.listViewCafe.TabIndex = 0;
             this.listViewCafe.UseCompatibleStateImageBehavior = false;
             this.listViewCafe.View = System.Windows.Forms.View.Details;
@@ -110,10 +112,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(947, 498);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(710, 405);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(257, 52);
+            this.button1.Size = new System.Drawing.Size(193, 42);
             this.button1.TabIndex = 1;
             this.button1.Text = "Show Info";
             this.button1.UseVisualStyleBackColor = true;
@@ -121,28 +123,47 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(63, 498);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(47, 405);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(283, 52);
+            this.button2.Size = new System.Drawing.Size(212, 42);
             this.button2.TabIndex = 2;
             this.button2.Text = "Close";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(299, 409);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(482, 410);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 18);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Search by Name";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // ListForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1261, 578);
+            this.ClientSize = new System.Drawing.Size(946, 470);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listViewCafe);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ListForm";
             this.Text = "ListForm";
             this.Load += new System.EventHandler(this.ListForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -160,5 +181,7 @@
         private System.Windows.Forms.ColumnHeader columnWorkdays;
         private System.Windows.Forms.ColumnHeader columnSaturday;
         private System.Windows.Forms.ColumnHeader columnSunday;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
