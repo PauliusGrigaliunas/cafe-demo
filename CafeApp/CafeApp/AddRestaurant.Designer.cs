79f43351501f84285c6e29ac0357bae0474628d4
Karolis Staskevičius <staskeviciuskarolis@gmail.com>
2017-09-28 20:56:19 +0300

    AddRestaurant window no longer crashes on invalid information input Login shortcut for testing Unused Kavine class deleted

--- CafeApp/CafeApp/AddRestaurant.Designer.cs
+++ CafeApp/CafeApp/AddRestaurant.Designer.cs
@@ -39,7 +39,6 @@
             this.label9 = new System.Windows.Forms.Label();
             this.NameBox = new System.Windows.Forms.TextBox();
             this.AddressBox = new System.Windows.Forms.TextBox();
-            this.TablesBox = new System.Windows.Forms.TextBox();
             this.PhoneBox = new System.Windows.Forms.TextBox();
             this.WorkdaysBox = new System.Windows.Forms.TextBox();
             this.SaturdayBox = new System.Windows.Forms.TextBox();
@@ -47,16 +46,17 @@
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.AddButton = new System.Windows.Forms.Button();
+            this.TablesBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.TablesBox)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label1.Location = new System.Drawing.Point(192, 25);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(144, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(163, 25);
+            this.label1.Size = new System.Drawing.Size(135, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Add a Restaurant";
             // 
@@ -64,10 +64,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label2.Location = new System.Drawing.Point(92, 86);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(69, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.Size = new System.Drawing.Size(95, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Cafe\'s name:*";
             // 
@@ -75,10 +74,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label3.Location = new System.Drawing.Point(71, 119);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(53, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(133, 20);
+            this.label3.Size = new System.Drawing.Size(111, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Cafe\'s address:*";
             // 
@@ -86,10 +84,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label4.Location = new System.Drawing.Point(45, 151);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(34, 123);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(155, 20);
+            this.label4.Size = new System.Drawing.Size(130, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "Cafe\'s table count:*";
             // 
@@ -97,10 +94,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label5.Location = new System.Drawing.Point(35, 187);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(26, 152);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(159, 20);
+            this.label5.Size = new System.Drawing.Size(138, 17);
             this.label5.TabIndex = 4;
             this.label5.Text = "Your phone number:";
             // 
@@ -108,10 +104,9 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label6.Location = new System.Drawing.Point(223, 228);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(167, 185);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(306, 20);
+            this.label6.Size = new System.Drawing.Size(256, 17);
             this.label6.TabIndex = 5;
             this.label6.Text = "Working hours:*(format: hh:mm-hh:mm)";
             // 
@@ -119,10 +114,9 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label7.Location = new System.Drawing.Point(45, 267);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(34, 217);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(83, 20);
+            this.label7.Size = new System.Drawing.Size(71, 17);
             this.label7.TabIndex = 6;
             this.label7.Text = "Workdays";
             // 
@@ -130,10 +124,9 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label8.Location = new System.Drawing.Point(260, 267);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(195, 217);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(75, 20);
+            this.label8.Size = new System.Drawing.Size(65, 17);
             this.label8.TabIndex = 7;
             this.label8.Text = "Saturday";
             // 
@@ -141,113 +134,117 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label9.Location = new System.Drawing.Point(469, 267);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Location = new System.Drawing.Point(352, 217);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(64, 20);
+            this.label9.Size = new System.Drawing.Size(56, 17);
             this.label9.TabIndex = 8;
             this.label9.Text = "Sunday";
             // 
             // NameBox
             // 
-            this.NameBox.Location = new System.Drawing.Point(227, 86);
-            this.NameBox.Margin = new System.Windows.Forms.Padding(4);
+            this.NameBox.Location = new System.Drawing.Point(170, 70);
             this.NameBox.Name = "NameBox";
-            this.NameBox.Size = new System.Drawing.Size(255, 22);
+            this.NameBox.Size = new System.Drawing.Size(192, 20);
             this.NameBox.TabIndex = 9;
             this.NameBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NameBox_KeyDown);
             // 
             // AddressBox
             // 
-            this.AddressBox.Location = new System.Drawing.Point(227, 119);
-            this.AddressBox.Margin = new System.Windows.Forms.Padding(4);
+            this.AddressBox.Location = new System.Drawing.Point(170, 97);
             this.AddressBox.Name = "AddressBox";
-            this.AddressBox.Size = new System.Drawing.Size(255, 22);
+            this.AddressBox.Size = new System.Drawing.Size(192, 20);
             this.AddressBox.TabIndex = 10;
             this.AddressBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AddressBox_KeyDown);
             // 
-            // TablesBox
-            // 
-            this.TablesBox.Location = new System.Drawing.Point(227, 153);
-            this.TablesBox.Margin = new System.Windows.Forms.Padding(4);
-            this.TablesBox.Name = "TablesBox";
-            this.TablesBox.Size = new System.Drawing.Size(255, 22);
-            this.TablesBox.TabIndex = 11;
-            this.TablesBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TablesBox_KeyDown);
-            // 
             // PhoneBox
             // 
-            this.PhoneBox.Location = new System.Drawing.Point(227, 187);
-            this.PhoneBox.Margin = new System.Windows.Forms.Padding(4);
+            this.PhoneBox.Location = new System.Drawing.Point(170, 152);
             this.PhoneBox.Name = "PhoneBox";
-            this.PhoneBox.Size = new System.Drawing.Size(255, 22);
+            this.PhoneBox.Size = new System.Drawing.Size(192, 20);
             this.PhoneBox.TabIndex = 12;
             this.PhoneBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PhoneBox_KeyDown);
             // 
             // WorkdaysBox
             // 
-            this.WorkdaysBox.Location = new System.Drawing.Point(49, 311);
-            this.WorkdaysBox.Margin = new System.Windows.Forms.Padding(4);
+            this.WorkdaysBox.Location = new System.Drawing.Point(37, 253);
             this.WorkdaysBox.Name = "WorkdaysBox";
-            this.WorkdaysBox.Size = new System.Drawing.Size(71, 22);
+            this.WorkdaysBox.Size = new System.Drawing.Size(54, 20);
             this.WorkdaysBox.TabIndex = 13;
             this.WorkdaysBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.WorkdaysBox_KeyDown);
             // 
             // SaturdayBox
             // 
-            this.SaturdayBox.Location = new System.Drawing.Point(264, 311);
-            this.SaturdayBox.Margin = new System.Windows.Forms.Padding(4);
+            this.SaturdayBox.Location = new System.Drawing.Point(198, 253);
             this.SaturdayBox.Name = "SaturdayBox";
-            this.SaturdayBox.Size = new System.Drawing.Size(68, 22);
+            this.SaturdayBox.Size = new System.Drawing.Size(52, 20);
             this.SaturdayBox.TabIndex = 14;
             this.SaturdayBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SaturdayBox_KeyDown);
             // 
             // SundayBox
             // 
-            this.SundayBox.Location = new System.Drawing.Point(473, 311);
-            this.SundayBox.Margin = new System.Windows.Forms.Padding(4);
+            this.SundayBox.Location = new System.Drawing.Point(355, 253);
             this.SundayBox.Name = "SundayBox";
-            this.SundayBox.Size = new System.Drawing.Size(69, 22);
+            this.SundayBox.Size = new System.Drawing.Size(53, 20);
             this.SundayBox.TabIndex = 15;
             this.SundayBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SundayBox_KeyDown);
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(16, 53);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Location = new System.Drawing.Point(12, 43);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(118, 17);
+            this.label10.Size = new System.Drawing.Size(89, 13);
             this.label10.TabIndex = 16;
             this.label10.Text = "*neccessary to fill";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(375, 351);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Location = new System.Drawing.Point(281, 285);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(218, 17);
+            this.label11.Size = new System.Drawing.Size(165, 13);
             this.label11.TabIndex = 17;
             this.label11.Text = "(Blank space means do not work)";
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(252, 384);
-            this.AddButton.Margin = new System.Windows.Forms.Padding(4);
+            this.AddButton.Location = new System.Drawing.Point(189, 312);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(120, 28);
+            this.AddButton.Size = new System.Drawing.Size(90, 23);
             this.AddButton.TabIndex = 18;
             this.AddButton.Text = "Add";
             this.AddButton.UseVisualStyleBackColor = true;
             this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
+            // TablesBox
+            // 
+            this.TablesBox.Location = new System.Drawing.Point(170, 124);
+            this.TablesBox.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.TablesBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.TablesBox.Name = "TablesBox";
+            this.TablesBox.Size = new System.Drawing.Size(191, 20);
+            this.TablesBox.TabIndex = 19;
+            this.TablesBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
             // AddRestaurant
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.ClientSize = new System.Drawing.Size(596, 427);
+            this.ClientSize = new System.Drawing.Size(447, 347);
+            this.Controls.Add(this.TablesBox);
             this.Controls.Add(this.AddButton);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
@@ -255,7 +252,6 @@
             this.Controls.Add(this.SaturdayBox);
             this.Controls.Add(this.WorkdaysBox);
             this.Controls.Add(this.PhoneBox);
-            this.Controls.Add(this.TablesBox);
             this.Controls.Add(this.AddressBox);
             this.Controls.Add(this.NameBox);
             this.Controls.Add(this.label9);
@@ -267,10 +263,10 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AddRestaurant";
             this.Text = "AddRestaurant";
             this.Load += new System.EventHandler(this.AddRestaurant_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TablesBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -289,7 +285,6 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox NameBox;
         private System.Windows.Forms.TextBox AddressBox;
-        private System.Windows.Forms.TextBox TablesBox;
         private System.Windows.Forms.TextBox PhoneBox;
         private System.Windows.Forms.TextBox WorkdaysBox;
         private System.Windows.Forms.TextBox SaturdayBox;
@@ -297,5 +292,6 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.NumericUpDown TablesBox;
     }
 }
