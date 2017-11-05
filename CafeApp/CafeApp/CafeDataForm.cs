7d2415d40aba289f1069d6798fcf00d6d409ed7c
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2017-11-05 16:28:51 +0200

    -+

--- CafeApp/CafeApp/CafeDataForm.cs
+++ CafeApp/CafeApp/CafeDataForm.cs
@@ -16,7 +16,6 @@ namespace CafeApp
         public int id;
         SqlConnection connect = new SqlConnection("Server = tcp:covfefedb.database.windows.net, 1433; Initial Catalog = covfefe; Persist Security Info=False;User ID = { your_username }; Password={your_password}; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;");
 
-        Connector connector = new Connector();
         string checkingEmail;
         string email;
         public CafeDataForm(int id, string email)
